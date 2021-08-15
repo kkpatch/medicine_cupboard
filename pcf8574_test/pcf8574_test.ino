18c80a6728ab34bbdb21b7b8a86117ff65606862
kkengg <kkeng_patchara@outlook.com>
2021-08-15 11:25:26 +0700

    Create pcf8574_test.ino

---
+++ pcf8574_test/pcf8574_test.ino
@@ -0,0 +1,47 @@
+#include <PCF8574.h>
+
+// Set address ไว้ที่ 0x20 (จั๊ม 0 0 0)
+PCF8574 PCF(0x20);
+
+void setup() {
+  Serial.begin(115200);
+  PCF.pinMode(P0, OUTPUT);
+  PCF.pinMode(P1, OUTPUT);
+  PCF.pinMode(P2, OUTPUT);
+  PCF.pinMode(P3, OUTPUT);
+  PCF.pinMode(P4, OUTPUT);
+  PCF.pinMode(P5, OUTPUT);
+  PCF.pinMode(P6, OUTPUT);
+  PCF.pinMode(P7, OUTPUT);
+  PCF.begin();
+}
+
+void dWrite(int pin,bool state){
+    PCF.digitalWrite(pin, state);
+}
+
+void loop() {
+  dWrite(P0,LOW);
+  dWrite(P1,LOW);
+  dWrite(P2,LOW);
+  dWrite(P3,LOW);
+  dWrite(P4,LOW);
+  dWrite(P5,LOW);
+  dWrite(P6,LOW);
+  dWrite(P7,LOW);
+
+  Serial.print("LOW");
+  Serial.println();
+  delay(1000);
+  PCF.digitalWrite(P0, HIGH);
+  PCF.digitalWrite(P1, HIGH);
+  PCF.digitalWrite(P2, HIGH);
+  PCF.digitalWrite(P3, HIGH);
+  PCF.digitalWrite(P4, HIGH);
+  PCF.digitalWrite(P5, HIGH);
+  PCF.digitalWrite(P6, HIGH);
+  PCF.digitalWrite(P7, HIGH);
+  Serial.print("HIGH");
+  Serial.println();
+  delay(1000);
+}
