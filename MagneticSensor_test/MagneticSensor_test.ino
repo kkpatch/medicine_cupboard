72dcd58e08ddaf2ef33a2d65cec713780393ec06
kkpatch <31177288+kkpatch@users.noreply.github.com>
2021-08-16 13:18:26 +0700

    Update MagneticSensor_test.ino

--- MagneticSensor_test/MagneticSensor_test.ino
+++ MagneticSensor_test/MagneticSensor_test.ino
@@ -1,23 +1,22 @@
-int buttonState = 0;  
+int state; // 0 close - 1 open wwitch
 
-void setup() {
-  Serial.begin(9600);   
-  pinMode(D1, INPUT);     
+void setup()
+{
+  pinMode(5, INPUT_PULLUP);
+  Serial.begin (9600);
 }
 
-void loop(){
-
-  // read the state of the pushbutton value:
-
-  buttonState = digitalRead(D1);
-  if (buttonState == HIGH) {     
-    Serial.println("High"); 
-  } 
-
-  else {
-    Serial.println("Low");
+void loop()
+{
+  state = digitalRead(5);
+  
+  if (state == HIGH){
+    Serial.print("Not Connected");
+    Serial.println();
   }
-
- 
-
+  else{
+    Serial.print("Connected");
+    Serial.println();
+  }
+  delay(200);
 }
