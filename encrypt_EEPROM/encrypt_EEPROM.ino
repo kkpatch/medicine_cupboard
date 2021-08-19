64cd003c3646a6c70eb61a0b9b8711eee22dee37
kkpatch <31177288+kkpatch@users.noreply.github.com>
2021-08-19 08:58:38 +0700

    Create encrypt_EEPROM.ino

---
+++ encrypt_EEPROM/encrypt_EEPROM.ino
@@ -0,0 +1,13 @@
+String str="ABCDEFG0054013002301403113014502301";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.print(str[0]);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
